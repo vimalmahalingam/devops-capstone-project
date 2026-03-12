baec811a5d15fa39b19099ece22fda0f9de9744f
vimalmahalingam <vimalmahalingam.m@gmail.com>
2026-03-12 20:34:51 +0530

    Update requirements.txt to include Flask
    Removed unittest and test class for hello function.

--- test_app.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from app import hello
-
-class TestApp(unittest.TestCase):
-    def test_hello(self):
-        self.assertEqual(hello(), "Hello, DevOps Capstone Project!")
-
-if __name__ == "__main__":
-    unittest.main()
