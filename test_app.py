d226ba01f45bb94819089a165e6f26247dd485e6
vimalmahalingam <vimalmahalingam.m@gmail.com>
2026-03-12 20:42:54 +0530

    Refactor tests to use Flask app client

--- test_app.py
+++ test_app.py
@@ -1,9 +1,14 @@
 import unittest
-from app import hello
+from app import app
 
-class TestApp(unittest.TestCase):
-    def test_hello(self):
-        self.assertEqual(hello(), "Hello, CI/CD World!")
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
 
-if __name__ == "__main__":
+    def test_hello_route(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), "Hello, DevOps Capstone Project!")
+
+if __name__ == '__main__':
     unittest.main()
